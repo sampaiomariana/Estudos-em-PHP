849b4a3a63a36e1727e9483c6372e43fe6f9d652
mariana <marisampa26@gmail.com>
2021-01-19 18:54:16 -0300

    basic php

---
+++ php básico/aula_01/index.php
@@ -0,0 +1,17 @@
+<html>
+
+    <head>
+        <title> Website </title>
+        <meta charset = "utf-8">
+    </head>
+
+    <body>
+        <p> 
+        
+        <?php
+        echo "Hello world"; 
+        ?>
+        </p>
+    </body>
+
+</html>
---
+++ php básico/aula_03/index.php
@@ -0,0 +1,31 @@
+<?php
+
+/*Variáveis*/
+/*String*/ 
+$name  = 'Mariana <br />';
+echo $name;
+
+/*Inteiro*/
+$age  = '25 <br />';
+echo $age;
+
+$profession = 'computer engineer <br />';
+echo $profession;
+$bool = true;
+
+$double  = 10.09;
+
+/*Verdadeiro - 1
+  False - 0
+*/
+echo $bool;
+echo $double;
+
+/*Constantes - declara apenas uma vez */
+
+define('NOME','Mariana');
+
+echo NOME;
+
+
+?>
