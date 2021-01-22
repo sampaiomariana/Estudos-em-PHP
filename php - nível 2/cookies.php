60581189e98e7a1e3c0f6db43e186c40ebbbd7f6
Mariana Sampaio <43734115+sampaiomariana@users.noreply.github.com>
2021-01-22 10:17:25 -0300

    session and cookies

---
+++ php - nível 2/cookies.php
@@ -0,0 +1,17 @@
+<?php
+
+    /*cookie -> nós decidimor por quanto tempo pode ficar válido o cookie.
+    */
+
+    session_start();
+    setcookie('nome', 'Mariana', time() + (60*60*24), '/');
+
+    echo $_COOKIE['nome'];
+
+    //Para destruir , colocar um tempo negativo.
+
+
+
+
+
+?>
---
+++ php - nível 2/session.php
@@ -0,0 +1,15 @@
+<?php
+ /* session -> é quando armazenamos um valor a variável e mesmo quando o usuário fecha a aba
+    do navegador ao retornar a variável ainda está guardada. Ele só encerra quando o usuário
+    fecha o navegador.
+*/
+
+session_start();
+//destruir a sessão
+session_destroy();
+
+
+
+
+
+?>
