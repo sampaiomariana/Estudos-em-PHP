71db126ba2c6c66b557e9d091f6ce6e49fd6a6ec
Mariana Sampaio <43734115+sampaiomariana@users.noreply.github.com>
2021-01-25 11:03:41 -0300

    add inner join

--- banco de dados/estrutura2.sql
+++ banco de dados/estrutura2.sql
@@ -2,9 +2,16 @@ create DATABASE acesso;
 
 use acesso;
 
+
+create table cargos(
+
+    id  SERIAL PRIMARY KEY,
+    nome_cargo  VARCHAR(255)
+);
+
 create TABLE clientes (
     id  SERIAL PRIMARY KEY,
     nome    VARCHAR(255),
-    cargo   INTEGER
+    cargo   INTEGER REFERENCES cargos(id);
 
 );
--- banco de dados/group.php
+++ banco de dados/group.php
@@ -18,8 +18,24 @@
         echo '<hr>';
     }
 
-    
-    
+    // left e right join 
+    $pdo = new PDO('mysql:host=localhost;dbname =estrutura2', 'root', '' );
+    //Visualizar os erros 
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = $pdo->prepare("SELECT * FROM `clientes` LEFT JOIN `cargos` ON `clientes`.`id` 
+    = `cargos`.`id`");
+    $sql->execute();
+    $clientes = $sql -> fetchAll();
+    foreach ($clientes as $key => $value){
+        echo $value ['nome'];
+        echo '|';
+        echo $value['nome_cargo'];
+        echo '<hr>';
+    }
+
+    // Inner join só retorna o que o id bate com o id do cargo
+    // left join retorna o cliente mesmo que não bata - independente dos cargos vazios retorna tudo dos clientes
+    //right join retorna apenas o que bate dos cargos - retorna todos os cargos independete do cliente
    
 
 ?>
